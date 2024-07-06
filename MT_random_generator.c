837bb29d7ba6d528922ebbf0200b61cd12c6d10b
Álvaro Morales <moralesalvaro97@gmail.com>
2024-07-06 20:50:50 +0200

    mersenne-twister return values in [0-1]

--- MT_random_generator.c
+++ MT_random_generator.c
@@ -3,12 +3,14 @@
 #include <unistd.h>
 #include "lib/include/libft.h"
 
-static MTrand  *get_rand_generator(int seed)
+static MTrand  *get_rand_generator(unsigned int seed)
 {
-    MTrand  *rand;
-    int     value;
+    MTrand          *rand;
+    unsigned int     value;
 
     rand = (MTrand*) malloc(sizeof(MTrand));
+    if (!rand)
+        return (NULL);
     rand->index = 1;
     rand->vector[0] = seed & 0xffffffff;
     while (rand->index < N)
@@ -30,24 +32,23 @@ static int tempering(int y)
     return (y);
 }
 
-void    next_state_value(MTrand *rand, int k1, int *a, int distance)
+void    next_state_value(MTrand *rand, int k1, int k, int *a, int distance)
 {
-    int y;
-    int rand_m;
+    unsigned int y;
+    unsigned int rand_m;
 
-    rand->vector[k1 - 1] & UPPER_MASK;
-    y = (rand->vector[k1 - 1] & UPPER_MASK)
+    y = (rand->vector[k] & UPPER_MASK)
         | (rand->vector[k1] & LOWER_MASK);
-    rand_m = rand->vector[(k1 - 1 + distance) % N];
-    rand->vector[k1 - 1] = rand_m ^ (y >> 1) ^ a[y & 0x1];
+    rand_m = rand->vector[(k + distance)];
+    rand->vector[k] = rand_m ^ (y >> 1) ^ a[y & 0x1];
 }
 
 float MT_recursion(MTrand *rand)
 {
-    int y;
-    int k;
-    int k1;
-    int a[2];
+    float y;
+    unsigned int k;
+    unsigned int k1;
+    unsigned int a[2];
 
     a[0] = 0x0;
     a[1] = A_M;
@@ -55,41 +56,50 @@ float MT_recursion(MTrand *rand)
     while (k < N - M)
     {
         k1 = (k + 1) % N;
-        next_state_value(rand, k1, a, M);
+        next_state_value(rand, k1, k, a, M);
         k = k1;
     }
-    while (k < N - 1)
+    while (k < N-1)
     {
         k1 = (k + 1) % N;
-        next_state_value(rand, k1, a, M - N);
+        next_state_value(rand, k1, k, a, M - N);
         k = k1;
     }
-    y = (float) tempering(rand->vector[k]);
-    //y /= 4294967295;
+    next_state_value(rand, k1, k, a, M - N);
+    y = (float) tempering(rand->vector[N-1]);
+    y /= 4294967295;
     return (y);
 }
 
-int get_random_value(void)
+float get_random_value(int seed)
 {
-    static MTrand   *rand;
-    int             y;
+    static MTrand  *rand;
+    float           y;
+    unsigned int    s;
 
-    if (rand)
+    if (seed <= 0)
+        s = INITIAL_SEED;
+    if (!rand)
+    {
         rand = get_rand_generator(INITIAL_SEED);
+        if (!rand)
+            return (-1);
+    }
     y = MT_recursion(rand);
     return (y);
 }
 
 int main(void)
 {
-    int             next_number;
+    float   next_number;
+    MTrand  *rand;
 
-    next_number = get_random_value();
-    printf("next_number: %d\n", next_number);
-    next_number = get_random_value();
-    printf("next_number: %d\n", next_number);
-    next_number = get_random_value();
-    printf("next_number: %d\n", next_number);
-    next_number = get_random_value();
-    printf("next_number: %d\n", next_number);
+    next_number = get_random_value(0);
+    if (next_number == -1)
+        return (0);
+    printf("next_number: %f\n", next_number);
+    next_number = get_random_value(0);
+    printf("next_number: %f\n", next_number);
+    next_number = get_random_value(0);
+    printf("next_number: %f\n", next_number);
 }
