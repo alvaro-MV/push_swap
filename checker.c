69bb8baa4e08353276378d80b46badf43e07ad39
Álvaro Morales <moralesalvaro97@gmail.com>
2024-08-13 02:12:20 +0200

    checker.c file added; instructions for building the checker

---
+++ checker.c
@@ -0,0 +1,17 @@
+/*
+	-Implementar get_next_line; adaptar respecto de pipex.
+
+	-Definir una función que asocie a cada posible instruccion que funcion
+		se ha de ejecutar.
+	
+	-Parsear los argumentos: reutilizar stack_io (solo read_list) y  is_not_valid
+		y parse_args. Generar un stack.
+	-while (get next line) --> instruccion: con el mapa, ejecutas la función que toque.
+		Si no existe esa instrucción a la verga.
+	
+	-Reutilizar is_sorted para comprobar que está ordenado. 
+*/
+
+int	main(int argc, char **argv)
+{
+}
